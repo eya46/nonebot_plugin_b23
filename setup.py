283b521b72810b7640a0dacb0d4b6a7fccdf74b8
eya46 <644310932@qq.com>
2023-02-01 19:24:35 +0800

    去除注释

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import setuptools  # 导入setuptools打包工具
+import setuptools
 
 setuptools.setup(
     name="nonebot-plugin-b23",
@@ -9,15 +9,15 @@ setuptools.setup(
         'nonebot2>=2.0.0-beta.1',
         'pydantic~=1.10.4'
     ],
-    author_email="eya46@qq.com",  # 作者联系方式，可写自己的邮箱地址
+    author_email="eya46@qq.com",
     description="nonebot2 plugin B站热搜",  # 包的简述
     long_description_content_type="text/markdown",
-    url="https://github.com/eya46/nonebot_plugin_b23",  # 自己项目地址，比如github的项目地址
+    url="https://github.com/eya46/nonebot_plugin_b23",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',  # 对python的最低版本要求
+    python_requires='>=3.8'
 )
