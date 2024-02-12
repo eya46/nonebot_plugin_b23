10187072814bb7f7dbde6a7953b9c50bb9f22201
eya46 <644310932@qq.com>
2024-02-12 22:43:42 +0800

    兼容pydantic V2

--- nonebot_plugin_b23/config.py
+++ nonebot_plugin_b23/config.py
@@ -6,9 +6,10 @@ from pydantic import BaseModel, Field
 
 class Config(BaseModel):
     b23_default_command: str = Field(default='B站热搜')
-    b23_commands: Set[str] = Field(default={'b23', })
+    b23_commands: Set[str] = Field(default_factory=lambda: {'b23', })
     b23_block: bool = Field(default=False)
     b23_priority: int = Field(default=99)
     b23_max_length: int = Field(default=20, gt=0, lt=101)
+
 
 config = Config(**get_driver().config.dict())
